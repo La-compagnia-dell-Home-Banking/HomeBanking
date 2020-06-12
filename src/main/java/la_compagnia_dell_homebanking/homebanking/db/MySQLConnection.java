a957e60f6df3cd7c192b70f9d718dd93cf4bc928
oleskiy OS <12lie.to.me04@gmail.com>
2020-06-12 21:58:13 +0200

    add DatabaseTest for simple Junit connection tests

--- src/main/java/la_compagnia_dell_homebanking/homebanking/db/MySQLConnection.java
+++ src/main/java/la_compagnia_dell_homebanking/homebanking/db/MySQLConnection.java
@@ -4,22 +4,28 @@ import java.sql.*;
 
 public class MySQLConnection {
 
-    private Statement stmt = null;
-    private Connection conn = null;
+    private final Statement STMT;
+    private final Connection CONN;
 
     public MySQLConnection() throws SQLException {
         String password = System.getenv("passhomebanking");
-        this.conn = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7347764?user=sql7347764&password=" +
-                password);
-        stmt = conn.createStatement();
+        this.CONN = DriverManager.getConnection("jdbc:mysql://sql7.freemysqlhosting.net:3306/sql7347764?" +
+                "user=sql7347764&password=" + password);
+        STMT = CONN.createStatement();
     }
 
-    public Statement getStmt() {
-        return stmt;
+    public MySQLConnection(String username, String password, String db_port, String db_url, String db_name, String db_type) throws SQLException {
+        this.CONN = DriverManager.getConnection("jdbc:" + db_type +":" + db_url + ":" + db_port + "/"
+                + db_name + "?user=" + username + "&password=" + password);
+        STMT = CONN.createStatement();
+    }
+
+    public Statement getSTMT() {
+        return this.STMT;
     }
 
     public Connection connect() {
-        return this.conn;
+        return this.CONN;
     }
 
 }
