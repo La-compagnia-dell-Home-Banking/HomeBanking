055778463f1853747cc4b86b3896bef15b5c4796
oleskiy OS <12lie.to.me04@gmail.com>
2020-06-15 16:57:20 +0200

    add class PersonaQueries for database queries

--- src/main/java/la_compagnia_dell_homebanking/homebanking/db/PersonaQueries.java
+++ src/main/java/la_compagnia_dell_homebanking/homebanking/db/PersonaQueries.java
@@ -1,4 +1,51 @@
 package la_compagnia_dell_homebanking.homebanking.db;
 
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class PersonaQueries {
+
+    static boolean insertPersonToDb(PersFisica personaFisica, String persona_id) {
+        Boolean status = null;
+        MySQLConnection connection = new MySQLConnection();
+        String query = "INSERT INTO persona_fisica VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        try {
+            PreparedStatement prstmt = connection.getMyConnection().prepareStatement(query);
+            prstmt.setString(1, persona_id);
+            prstmt.setString(2, personaFisica.getNome());
+            prstmt.setString(3, personaFisica.getCognome());
+            prstmt.setString(4, personaFisica.getDocs().getCodice_fiscale());
+            prstmt.setString(5, personaFisica.getdataDiNascita());
+            prstmt.setString(6, personaFisica.getLuogoDiNascita());
+            prstmt.setString(7, personaFisica.getResidenza());
+            prstmt.setString(8, personaFisica.getIndirizzo());
+            prstmt.setString(9, personaFisica.getCap());
+            prstmt.setString(10, personaFisica.getEmail());
+            prstmt.setString(11, personaFisica.getTelefono());
+            prstmt.setString(12, personaFisica.getDocs().getDocument());
+            status = prstmt.execute();
+        } catch (SQLException e) {
+            System.out.println(new StringBuilder().append("SQLException: ").append(e.getMessage()));
+            System.out.println(new StringBuilder().append("SQLState: ").append(e.getSQLState()));
+            System.out.println(new StringBuilder().append("VendorError: ").append(e.getErrorCode()));
+        } finally {
+            try {
+                connection.getMyConnection().close();
+            } catch (SQLException e) {
+                System.out.println(new StringBuilder().append("SQLException: ").append(e.getMessage()));
+                System.out.println(new StringBuilder().append("SQLState: ").append(e.getSQLState()));
+                System.out.println(new StringBuilder().append("VendorError: ").append(e.getErrorCode()));
+            }
+
+        }
+        if(status != null && status) {
+            StringBuilder stringBuilder = new StringBuilder().append("Success. ").append(personaFisica.toString()).
+                    append(" was created.");
+            System.out.println(stringBuilder);
+        }
+        return status;
+    }
+
+//    public List<>
+
 }
