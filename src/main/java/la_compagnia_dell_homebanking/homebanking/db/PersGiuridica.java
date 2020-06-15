e6bb088c90010fd6349dd64060c11027620a62b4
oleskiy OS <12lie.to.me04@gmail.com>
2020-06-15 17:16:29 +0200

    ultimi

--- src/main/java/la_compagnia_dell_homebanking/homebanking/db/PersGiuridica.java
+++ src/main/java/la_compagnia_dell_homebanking/homebanking/db/PersGiuridica.java
@@ -4,14 +4,20 @@ public class PersGiuridica extends Persona {
 
 	private String ragione_sociale;
 	private long p_iva;
+	private String nomeRappresentante;
 
 
 	public PersGiuridica(String nome, String telefono, String email, String codice_fiscale, String ragione_sociale, long p_iva,
-						 String sede, String document, String cap) {
-		super(nome, telefono, email, sede, document, cap);
+						 String sede, String document, String cap, String nomeRappresentante) {
+		super(nome, telefono, email, sede, document, cap, NumberGenerator.generateRandom());
 		this.p_iva = p_iva;
 		this.ragione_sociale = ragione_sociale;
+		this.nomeRappresentante = nomeRappresentante;
 		this.getDocs().setP_iva(p_iva);
+	}
+
+	public String getNomeRappresentante() {
+		return nomeRappresentante;
 	}
 
 	public String getRagione_sociale() {
