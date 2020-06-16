0e5a33d0dc47d0491cc604188ad451e80a1850ce
dim89 <66360214+dim89@users.noreply.github.com>
2020-06-16 15:44:19 +0200

    gestione carte, token e transazioni

--- src/main/java/la_compagnia_dell_homebanking/homebanking/db/CartaI.java
+++ src/main/java/la_compagnia_dell_homebanking/homebanking/db/CartaI.java
@@ -1,10 +1,11 @@
 package la_compagnia_dell_homebanking.homebanking.db;
 
+import java.sql.SQLException;
 import java.time.LocalDate;
 
 public interface CartaI {
 	
-	public String getAccountId();
+	public String getAccountId() throws SQLException;
 	public String getNumeroCarta();
 	public LocalDate getDataScadenza();
 	public String getCvv();
