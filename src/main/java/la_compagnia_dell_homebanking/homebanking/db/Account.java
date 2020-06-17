be895f52931df160513613ad5ebaff63b60b1367
AleDinve <66356297+AleDinve@users.noreply.github.com>
2020-06-17 11:47:58 +0200

    Account e ContoCorrente migliorati

--- src/main/java/la_compagnia_dell_homebanking/homebanking/db/Account.java
+++ src/main/java/la_compagnia_dell_homebanking/homebanking/db/Account.java
@@ -3,18 +3,35 @@ package la_compagnia_dell_homebanking.homebanking.db;
 import java.sql.*;
 import java.util.*;
 
-
-
 public class Account {
-	private static int accountID; 
+	private ArrayList<ContoCorrente> lista_conti;
+	private static int accountID;
 	private final Persona persona;
 	private String password = "";
 
 	public Account(Persona persona) throws SQLException {
-		accountID=controlAccount_id()+1;
+		accountID = controlAccount_id() + 1;
 		this.persona = persona;
 		this.setPassword();
 		this.insertAccountToDb();
+		lista_conti = ContoCorrente.readConti();
+	}
+
+	public Account(String account_id) throws SQLException {
+		accountID = Integer.parseInt(account_id);
+		MySQLConnection connection = new MySQLConnection();
+		String query = "SELECT * from account WHERE account_id=" + account_id;
+		PreparedStatement prstmt = connection.getMyConnection().prepareStatement(query);
+		ResultSet rs = prstmt.executeQuery();
+		rs.next();
+		String persFis = rs.getString("persona_id");
+		String persGiur = rs.getString("azienda_id");
+		if (persFis != null)
+			this.persona = getPersonaFisicaFromDb(persFis);
+		else
+			this.persona = getPersonaGiuridicaFromDb(persGiur);
+		this.setPassword();
+
 	}
 
 	private boolean insertAccountToDb() throws SQLException {
@@ -35,24 +52,21 @@ public class Account {
 		connection.getMyConnection().close();
 		return status;
 	}
-	
-	
+
 	private static int controlAccount_id() throws SQLException {
 		MySQLConnection connection = new MySQLConnection();
 		String query = "SELECT * from account";
 		PreparedStatement prstmt = connection.getMyConnection().prepareStatement(query);
 		ResultSet rs = prstmt.executeQuery();
-		int s,id=0;
+		int s, id = 0;
 		while (rs.next()) { // Leggiamo i risultati
-			s=Integer.parseInt(rs.getString("account_id"));
-			if (id<s)
-				id=s;
+			s = Integer.parseInt(rs.getString("account_id"));
+			if (id < s)
+				id = s;
 
 		}
 		return id;
-		
-	
-	
+
 	}
 
 	private void setPassword() {
@@ -65,5 +79,48 @@ public class Account {
 	public String getPassword() {
 		return this.password;
 	}
+
+	public int getAccountID() {
+		return this.accountID;
+	}
+
+	public boolean aggiungiConto(ContoCorrente conto) throws SQLException {
+		if (lista_conti.contains(conto))
+			return false;
+		if (!this.equals(conto.getAccount()))
+			return false;
+		boolean s = conto.insertCCToDb(this);
+		return lista_conti.add(conto);
+
+	}
+
+	private Persona getPersonaFisicaFromDb(String pers) throws SQLException {
+
+		MySQLConnection connection = new MySQLConnection();
+		String query = "SELECT * FROM persona_fisica WHERE persona_id=" + pers;
+		PreparedStatement prstmt = connection.getMyConnection().prepareStatement(query);
+		ResultSet rs = prstmt.executeQuery();
+		rs.next();
+		PersFisica persona = new PersFisica(rs.getString("nome"), rs.getString("cognome"), rs.getString("telefono"),
+				rs.getString("email"), rs.getString("codice_fiscale"), rs.getString("data_nascita"),
+				rs.getString("luogo_nascita"), rs.getString("indirizzo"), rs.getString("documento"),
+				rs.getString("residenza"), rs.getString("cap"), pers);
+		return persona;
+	}
+
+	private Persona getPersonaGiuridicaFromDb(String pers) throws SQLException {
+		MySQLConnection connection = new MySQLConnection();
+		String query = "SELECT * FROM persona_giuridica WHERE azienda_id=" + pers;
+		PreparedStatement prstmt = connection.getMyConnection().prepareStatement(query);
+		ResultSet rs = prstmt.executeQuery();
+		rs.next();
+		PersGiuridica persona = new PersGiuridica(rs.getString("nome"), rs.getString("telefono"), rs.getString("email"),
+				rs.getString("codice_fiscale"), rs.getString("ragione_sociale"),
+				Long.parseLong(rs.getString("partita_iva")), rs.getString("sede_legale"), rs.getString("documento"),
+				rs.getString("cap"), rs.getString("nome_rappresentante"));
+		return persona;
+	}
+	
+
 
 }
