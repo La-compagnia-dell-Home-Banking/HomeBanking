fc01602788a3467dda369f412a2301ca3ca5246f
dim89 <66360214+dim89@users.noreply.github.com>
2020-06-18 10:24:45 +0200

    commenti vari sui metodi delle carte e implementazione della carta di credito

--- src/test/java/la_compagnia_dell_homebanking/homebanking/db/TestCarte.java
+++ src/test/java/la_compagnia_dell_homebanking/homebanking/db/TestCarte.java
@@ -5,8 +5,9 @@ import java.sql.SQLException;
 public class TestCarte {
 
 	public static void main(String[] args) throws SQLException {
-		Carta_Prepagata x=new Carta_Prepagata("8671204139373831");
-		x.pagaConCarta(100, "8671204139373831");
+		
+		
+
 
 	}
 
